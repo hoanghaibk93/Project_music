1f7dfeb4f9dd024eb1cd2507f0d8f482e141139b
hoanghaibk93 <hoanghaibk93@gmail.com>
2023-10-21 10:07:48 +0700

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render("home")
+  handleHomePage() {
+    const message = this.appService.getHello();
+    return {
+      message: message
+    }
+    // return "this.appService.getHello()";
   }
 }
