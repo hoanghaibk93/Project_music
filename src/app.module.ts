028d138ec97dd3e83600ffcc74ba0c6c4e9a7992
hoanghaibk93 <hoanghaibk93@gmail.com>
2023-10-21 14:33:07 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://hoanghaibk93:hoanghaibk93@cluster0.qiup1zv.mongodb.net/')],
   controllers: [AppController],
   providers: [AppService],
 })
